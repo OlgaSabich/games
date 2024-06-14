9bfa5000bdd9515f8697e9a090b54af5078e3d8e
Volha <olgasabich@mail.ru>
2024-06-14 20:50:55 +0300

    add games

--- games.js
+++ games.js
@@ -1,4 +1,4 @@
-const links = ['https://wildermyth.itch.io/wildermyth-omenroad','https://iced-lemon.itch.io/ballet-of-steel','https://zaratustra.itch.io/zetas-world'];
+const links = ['https://impulsegamestudios.itch.io/sms-99','https://danthai.itch.io/first-winter','https://joetheitchpro.itch.io/go-to-bed','https://apish-box-games.itch.io/lamp-post','https://hadriandev.itch.io/rinse-and-repeat','https://ferryliquid.itch.io/crowded-followed'];
 
 document.getElementById('Random').addEventListener('click', function() {  
-window.open(links[Math.floor(Math.random() * links.length)]); });
+window.open(links[Math.floor(Math.random() * links.length)], '_blank'); });
