d2a2df6e76f2fd064cfd7f7a1f0b5d9db0c70107
Volha <olgasabich@mail.ru>
2024-06-14 20:02:07 +0300

    change html

--- games.js
+++ games.js
@@ -1,7 +1,4 @@
-const page = document.querySelector('.Random');
-const links = ['https://raycastly.itch.io/clap-clap','https://kajik.itch.io/mourningtide'];
+const links = ['https://wildermyth.itch.io/wildermyth-omenroad','https://iced-lemon.itch.io/ballet-of-steel','https://zaratustra.itch.io/zetas-world'];
 
-page.onclick = function(event) {
-  event.preventDefault();
-  window.location.href = links[Math.floor(Math.random() * links.length)];
-};
+document.getElementById('Random').addEventListener('click', function() {  
+window.open(links[Math.floor(Math.random() * links.length)]); });
